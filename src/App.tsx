688be38bb9772e9ef23a593a2e500db172d93397
Harry Carr <harrycarr64@hotmail.com>
2025-12-12 03:14:16 +0000

    Add Vercel Analytics integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Analytics } from "@vercel/analytics/react"
 
 import {
     ColorScheme,
@@ -35,6 +36,7 @@ function App() {
                         </Route>
                     </Routes>
                 </BrowserRouter>
+                <Analytics />
             </MantineProvider>
         </ColorSchemeProvider>
     )
