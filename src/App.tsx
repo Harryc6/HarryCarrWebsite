867663528fe8bb06d19dd0e77f91bb6e291172aa
Harry Carr <harrycarr64@hotmail.com>
2025-12-13 02:10:13 +0000

    Move Vercel Analytics component to index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Analytics } from "@vercel/analytics/react"
 
 import {
     ColorScheme,
@@ -36,7 +35,6 @@ function App() {
                         </Route>
                     </Routes>
                 </BrowserRouter>
-                <Analytics />
             </MantineProvider>
         </ColorSchemeProvider>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { inject } from "@vercel/analytics"
+import { Analytics } from "@vercel/analytics/react"
 import "./index.css"
 import "./font.css"
 import App from "./App"
-
-inject()
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
     <React.StrictMode>
         <App />
+        <Analytics />
     </React.StrictMode>
 )
